80af4a5ed602a81ae20fe227c5ebcaf9196e4cf8
Dylan Virgin <virg0010@algonquinlive.com>
2019-09-13 15:25:24 -0400

    commit

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const littleRed = {
-
+  colour: 'red',
+  nouns: ['cats', 'dogs', 'assassins', 'humans'],
+  adjective: 'minty',
+  exclamation: 'wow',
+  verbs: 'cried', 'running', 'killing'
 }
+
+console.log(littleRed)
