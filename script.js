09b2ccd4d4303d04d0bdee76fdf789f61e18d058
Dylan Virgin <virg0010@algonquinlive.com>
2019-09-13 15:48:40 -0400

    commit

--- script.js
+++ script.js
@@ -3,7 +3,30 @@ const littleRed = {
   nouns: ['cats', 'dogs', 'assassins', 'humans'],
   adjective: 'minty',
   exclamation: 'wow',
-  verbs: 'cried', 'running', 'killing'
+  silly: 'booger',
+  verbs: ['cried', 'running', 'killing']
 }
 
-console.log(littleRed)
+console.log(`
+One day, Little ${littleRed.colour} Riding Hood was going through the forest carrying a 
+basket of ${littleRed.nouns[0]} for her grandmother. Suddenly, she met a big 
+${littleRed.adjective} wolf.
+
+"${littleRed.exclamation}" said the wolf. "Where are you going little ${littleRed.silly}?"
+
+"I'm going to my grandmother's house," she said. Then the wolf 
+${littleRed.verbs[0]} away.
+
+When Miss Riding Hood got to her grandmother's house, the wolf was in bed 
+dressed like her grandmother.
+
+"My, Grandmother," she said. "What big ${littleRed.nouns[1]} you have."
+
+"The better to ${littleRed.verbs[1]} you with," said the wolf.
+
+"And, Grandmother," she said, "what big ${littleRed.nouns[2]}" you have."
+
+The wolf said, "The better to ${littleRed.verbs[2]} you with."
+
+And then she said, "What big ${littleRed.nouns[3]}" you have, Grandmother." But 
+the wolf said nothing. He had just died of indigestion from eating Grandmother`)
